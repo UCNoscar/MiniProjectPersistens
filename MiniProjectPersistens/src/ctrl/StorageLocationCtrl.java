563775ae81ba48fa12659fd4d47ada1e2321f445
Oliver <10461251@ucn.dk>
2023-03-16 10:23:27 +0100

    Some preliminary work on StorageLocation

--- MiniProjectPersistens/src/ctrl/StorageLocationCtrl.java
+++ MiniProjectPersistens/src/ctrl/StorageLocationCtrl.java
@@ -1,5 +1,16 @@
 package ctrl;
 
-public class StorageLocationCtrl {
+import db.StorageLocationDBIF;
+import model.Product;
+import model.StorageLocation;
 
+public class StorageLocationCtrl {
+	private StorageLocationDBIF slDBIF;
+	public StorageLocation findStorageLocationByProduct(Product product) {
+		StorageLocation res = null;
+		
+		res = slDBIF.findStorageLocationByProduct(product);
+		
+		return res;
+	}
 }
